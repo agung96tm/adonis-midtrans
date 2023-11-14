00323459a42cdbc5c5ae4ec7101b152ff35a7e0e
Agung Yuliyanto <agung.96tm@gmail.com>
2023-11-14 19:11:38 +0700

    refactor by run prettier

--- adonis-typing/index.ts
+++ adonis-typing/index.ts
@@ -1,7 +1,7 @@
-declare module '@ioc:Agung96tm/Midtrans' {
-    export interface MidtransConfig {
-        isProduction: boolean
-        serverKey: string
-        clientKey: string
-    }
+declare module "@ioc:Agung96tm/Midtrans" {
+  export interface MidtransConfig {
+    isProduction: boolean;
+    serverKey: string;
+    clientKey: string;
+  }
 }
