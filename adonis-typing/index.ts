53c2f68543cedc1701a1621fece8f290f615d9cc
Agung Yuliyanto <agung.96tm@gmail.com>
2023-11-15 07:37:47 +0700

    adjustment adonis-typing, include export midtrans

--- adonis-typing/index.ts
+++ adonis-typing/index.ts
@@ -1,4 +1,8 @@
-declare module "@ioc:Agung96tm/Midtrans" {
+declare module '@ioc:Agung96tm/Midtrans' {
+  import { MidtransClient } from 'midtrans-node-client'
+  const midtrans: MidtransClient.Snap
+  export default midtrans
+
   export interface MidtransConfig {
     isProduction: boolean;
     serverKey: string;
